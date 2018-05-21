8e9b3690a00375130e7508dbee32140a67b27925
vinhdizzo <vinhdizzo@gmail.com>
2018-05-21 15:10:28 -0700

    First draft: version 0.0.1

---
+++ R/percentage_point_gap.R
@@ -0,0 +1,115 @@
+## setwd('DisImpact')
+## document()
+## setwd('..')
+## install('DisImpact')
+
+##' Calculate the Margin of Error (MOE) for the Percentage Point Gap (PPG) method.
+##'
+##' @title Margin of Error for the PPG
+##' @param n Sample size for the group of interest.
+##' @param proportion The proportion of successes for the group of interest.  If specified, then the proportion is used in the MOE formula.  Otherwise, a default proportion of 0.50 is used (conservative and yields the maximum MOE).
+##' @param min.moe The minimum MOE returned even if the sample size is large.  Defaults to 0.03.
+##' @return The margin of error for the PPG given the specified sample size.
+##' @export
+di_ppg_moe <- function(n, proportion, min.moe=0.03) {
+  if (missing(proportion)) {
+    return(max(1.96 * sqrt(0.25/n), min.moe))
+  }
+  else {
+    return(max(1.96 * sqrt(proportion * (1-proportion)/n), min.moe))
+  }
+}
+##' Calculate Disproportionate Impact per the Percentage Point Gap (PPG) method.
+##'
+##' This function determines Disproportionate Impact based on the Percentage Point Gap (PPG) method, as described in \href{http://extranet.cccco.edu/Portals/1/TRIS/Research/Analysis/PercentagePointGapMethod2017.pdf}{this} reference from the California Community Colleges Chancellor's Office.  It assumes that a higher rate is good ("success").  For rates that are deemed negative (eg, rate of drop-outs, high is bad), then consider looking at the converse of the non-success (eg, non drop-outs, high is good) instead in order to leverage this function properly.  Note that the margin of error (MOE) is calculated using using 1.96*sqrt(0.25^2/n), with a \code{min.moe} used as the minimum by default.
+##' @title Calculate Disproportionate Impact per the Percentage Point Gap (PPG) method.
+##' @param success A vector of success indicators (\code{1}/\code{0} or \code{TRUE}/\code{FALSE}) or an unquoted reference (name) to a column in \code{data} if it is specified.
+##' @param group A vector of group names of the same length as \code{success} or an unquoted reference (name) to a column in \code{data} if it is specified.
+##' @param cohort (Optional) A vector of cohort names of the same length as \code{success} or an unquoted reference (name) to a column in \code{data} if it specified.  Disproportionate Impact is calculated for every group within each cohort.  When \code{cohort} is not specified, then the analysis assumes a single cohort.
+##' @param reference Either \code{'overall'} (default), \code{'hpg'} (highest performing group), a single proportion (eg, 0.50), or a vector of proportions.  Reference is used as a point of comparison for Disproportionate Impact for each group.  When \code{cohort} is specified:\cr
+##'   1. \code{'overall'} will use the overall success rate of each cohort group as the reference;\cr
+##'   2. \code{'hpg'} will use the highest performing group in each cohort as reference;\cr
+##'   3. the specified proportion will be used for all cohorts;\cr
+##'   4. the specified vector of proportions will refer to the reference point for each cohort in alphabetical order (so the number of proportions should equal to the number of unique cohorts).\cr
+##' @param data (Optional) A data frame containing the variables of interest.  If \code{data} is specified, then \code{success}, \code{group}, and \code{cohort} will be searched within it.
+##' @param min.moe The minimum margin of error (MOE) to be used in the calculation of Disproportionate Impact and is passed to \link{ppg_moe}.  Defaults to \code{0.03}.
+##' @return A data frame consisting of: cohort (if used), group, n (sample size), success (number of successes for the cohort-group), pct (proportion of successes for the cohort-group), reference (reference used in DI calculation), moe (margin of error), pct.lo (lower 95\% confidence interval for pct), pct.hi (upper 95\% confidence interval for pct), and di.indicator (1 if there is Disproportionate Impact).
+##' @export
+##' @import dplyr rlang
+di_ppg <- function(success, group, cohort, reference=c('overall', 'hpg'), data, min.moe=0.03) {
+  ## require(magrittr)
+  ## require(dplyr)
+  ## require(rlang)
+
+  if (!missing(data)) {
+    eq_success <- enquo(success)
+    success <- data %>% select(!!eq_success) %>% unlist
+    eq_group <- enquo(group)
+    group <- data %>% select(!!eq_group) %>% unlist
+  }
+  
+  # Check if success is binary or logical and that there are no NA's
+  stopifnot(success %in% c(1, 0))
+
+  # Check if cohort is specified
+  if (missing(cohort)) {
+    cohort <- 1
+    remove_cohort <- TRUE
+  } else {
+    remove_cohort <- FALSE
+    if (!missing(data)) {
+      eq_cohort <- enquo(cohort)
+      cohort <- data %>% select(!!eq_cohort) %>% unlist
+    }
+  }
+
+  # Check if reference is specified: overall, hpg, or user-defined references
+  if (!is.numeric(reference)) {
+    reference <- match.arg(reference)
+    reference_type <- reference
+    reference_numeric <- 1
+  } else {
+    # expecting vector of length 1 or length equal to unique cohort
+    stopifnot(length(reference) == length(unique(cohort)))
+    reference_type <- 'custom'
+    reference_numeric <- reference
+    if (length(reference) > 1) {
+      dReference <- data_frame(cohort=sort(unique(cohort))
+                             , reference
+                               )
+    }
+  }
+
+  # Calculate
+  df <- data_frame(cohort, group, success)
+  dResults <- df %>%
+    group_by(cohort, group) %>%
+    summarize(n=n(), success=sum(success), pct=success/n) %>%
+    ungroup %>%
+    arrange(cohort, group)
+  if (!exists('dReference')) {
+    dResults <- dResults %>% 
+      group_by(cohort) %>%
+      mutate(reference=case_when(
+               reference_type=='overall' ~ sum(success) / sum(n)
+             , reference_type=='hpg' ~ max(pct)
+             , reference_type=='custom' ~ reference_numeric
+             )) %>%
+      ungroup
+  } else {
+    dResults <- dResults %>%
+      left_join(dReference)
+  }
+  dResults <- dResults %>% 
+    mutate(moe=di_ppg_moe(n, min.moe)
+         , pct.lo=pct - moe
+         , pct.hi=pct + moe
+         , di.indicator=ifelse(pct.hi < reference, 1, 0)
+           )
+  
+  if (remove_cohort) {
+    dResults$cohort <- NULL
+  }
+  
+  return(dResults)
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## README
+The `DisImpact` R package contains functions that help in determining Disproportionate Impact (DI) based on the following methodologies:
+
+1. [Percentage Point Gap](http://extranet.cccco.edu/Portals/1/TRIS/Research/Analysis/PercentagePointGapMethod2017.pdf) (PPG) method,
+2. [Proportionality Index](http://extranet.cccco.edu/Portals/1/TRIS/Research/Accountability/GUIDELINES%20FOR%20MEASURING%20DISPROPORTIONATE%20IMPACT%20IN%20EQUITY%20PLANS.pdf) method (method #1 in reference), and
+3. [80% Index](http://extranet.cccco.edu/Portals/1/TRIS/Research/Accountability/GUIDELINES%20FOR%20MEASURING%20DISPROPORTIONATE%20IMPACT%20IN%20EQUITY%20PLANS.pdf) (method #2 in reference).
+
+## Install Package
+```{r}
+# From CRAN
+## install.packages('DisImpact')
+
+# From github (Development)
+## devtools::install_github('vinhdizzo/DisImpact')
+```
