e4eef0f747ff3af11f319527c68ff2a687fa7483
Vinh Nguyen <vinhdizzo@gmail.com>
2020-08-24 09:42:52 -0700

    Fixed 80 index check

--- R/di_iterate.R
+++ R/di_iterate.R
@@ -49,7 +49,7 @@
 ##' @export
 di_iterate <- function(data, success_vars, group_vars, cohort_vars=NULL, scenario_repeat_by_vars=NULL, exclude_scenario_df=NULL, weight_var=NULL, include_non_disagg_results=TRUE, ppg_reference_groups='overall', min_moe=0.03, use_prop_in_moe=FALSE, prop_sub_0=0.5, prop_sub_1=0.5, di_prop_index_cutoff=0.80, di_80_index_cutoff=0.80, di_80_index_reference_groups='hpg', check_valid_reference=TRUE) {
   stopifnot(length(group_vars) == length(ppg_reference_groups) | length(ppg_reference_groups) == 1)
-  stopifnot(length(group_vars) == length(di_80_index_reference_groups) | is.na(di_80_index_reference_groups))
+  stopifnot(length(group_vars) == length(di_80_index_reference_groups) | (length(di_80_index_reference_groups) == 1 & di_80_index_reference_groups %in% c(NA, 'hpg')))
 
   # Check valid success_vars
   for (i in seq_along(success_vars)) {
